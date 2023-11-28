1b83d26b1d95fe3898225861c76d54766a622cee
Marat <mmnik84@mail.ru>
2023-11-28 22:20:16 +0700

    5. PsqlStore [#285209]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -69,7 +69,7 @@ public class HabrCareerParse implements Parse {
         return listPosts;
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Parse habr = new HabrCareerParse(new HabrCareerDateTimeParser());
         List<Post> postList = habr.list(SOURCE_LINK);
         for (Post post : postList) {
